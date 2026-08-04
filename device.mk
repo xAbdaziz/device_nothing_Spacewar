7aa7016429023ca49ebca1ba7d1cc726b66e3986
Abdulaziz <me@abdaziz.dev>
2026-08-04 23:09:11 +0300

    spacewar: PixelOS Bringup
    Change-Id: Iffda22762c7d7cbf9bdc6902f57225ba8af18543

--- device.mk
+++ device.mk
@@ -17,8 +17,7 @@ $(call inherit-product, vendor/nothing/Spacewar/Spacewar-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
     NcmTetheringOverlay
